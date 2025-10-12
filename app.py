14ec580e89be51603899aa1509b698380c173d99
sirutio <sirutio03@gmail.com>
2025-10-13 02:25:33 +0900

    Update app.py
    Add Indent

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-return "Hello my webpage!"
+  return "Hello my webpage!"
 
 if __name__ == "__main__":
-application.run(host='0.0.0.0', debug=True)
+  application.run(host='0.0.0.0', debug=True)

292f35247b38aa64728c5ae96902ad441ee69393
sirutio <sirutio03@gmail.com>
2025-10-13 02:22:39 +0900

    Update app.py
    return to slide code

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 import sys
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def hello():
 return "Hello my webpage!"
 
 if __name__ == "__main__":
-app.run(host='0.0.0.0', debug=True)
+application.run(host='0.0.0.0', debug=True)

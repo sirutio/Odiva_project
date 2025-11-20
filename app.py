6ec1c560bbdd8a6c52a0a464734e8fd0c9084197
haedam <haedam0710@ewha.ac.kr>
2025-11-21 02:58:22 +0900

    좋아요 버튼 기능 추가

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, session
+from flask import Flask, render_template, request, flash, redirect, url_for, session, jsonify
 from database import DBhandler
 import hashlib
 import math
@@ -122,6 +122,21 @@ def reg_review():
   return redirect(url_for('view_review'))
 
 
+# 좋아요 기능
+@application.route('/show_heart/<name>/', methods=['GET'])
+def show_heart(name):
+  my_heart = DB.get_heart_byname(session['id'], name)
+  return jsonify({'my_heart': my_heart})
+
+@application.route('/like/<name>/', methods=['POST'])
+def like(name):
+  my_heart = DB.update_heart(session['id'], 'Y', name)
+  return jsonify({'msg' : '좋아요 완료'})
+
+@application.route('/unlike/<name>/', methods=['POST'])
+def unlike(name):
+  my_heart = DB.update_heart(session['id'], 'N', name)
+  return jsonify({'msg' : '안좋아요 완료'})
 
 
 # reg_items
